f17de5eb4745e1d6b4e25b9316ce398e21a8487d
francoEmunoz <munozfranco011@gmail.com>
2022-10-03 17:21:13 -0300

    users route add

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const productRouter = require('./products')
+const userRouter = require('./users')
 
 router.use('/products', productRouter)
+router.use('/users', userRouter)
 
 module.exports = router;

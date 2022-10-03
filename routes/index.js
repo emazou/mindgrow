6741c89c709ad78f9b1ce739466d770fa29ab103
francoEmunoz <munozfranco011@gmail.com>
2022-10-03 19:07:53 -0300

    publications route add

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 const productRouter = require('./products')
 const userRouter = require('./users')
+const publicationRouter = require('./publications')
 
 router.use('/products', productRouter)
 router.use('/users', userRouter)
+router.use('/publications', publicationRouter)
 
 module.exports = router;

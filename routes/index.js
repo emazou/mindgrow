4e8d4b97c35d0981e0c3cf1d6b908830167bcbd7
Hector Garay <hectorgaraybascur@gmail.com>
2022-10-04 11:21:19 -0300

    adding publications route

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ const commentRouter = require('./comments')
 const publicationRouter = require('./publications')
 const reviewRouter = require('./reviews')
 
-router.use('/publications', publicationRouter)
 router.use('/products', productRouter)
 router.use('/users', userRouter)
 router.use('/comments', commentRouter)
+router.use('/publications', publicationRouter)
 router.use('/reviews', reviewRouter)
 
 module.exports = router;

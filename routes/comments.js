4dec21ec4c7e89155feeb47fa75ceb03d78fce74
Hector Garay <hectorgaraybascur@gmail.com>
2022-10-04 11:20:55 -0300

    adding publicationsid

--- routes/comments.js
+++ routes/comments.js
@@ -6,7 +6,7 @@ const { create, all, read, destroy, update } = require('../controllers/commentCo
 
 router.post('/', passport.authenticate("jwt", { session: false }), create)
 router.get('/', all)
-router.get('/:itineraryid', read)
+router.get('/:publicationsid', read)
 router.delete('/:id', passport.authenticate("jwt", { session: false }), destroy)
 router.patch('/:id', passport.authenticate("jwt", { session: false }), update)
 

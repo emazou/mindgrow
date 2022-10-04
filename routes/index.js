3cd0aa60aaf4f058e6237bb19291ed6f64feb1de
Hector Garay <hectorgaraybascur@gmail.com>
2022-10-04 10:46:13 -0300

    Adding route comments

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 const productRouter = require('./products')
 const userRouter = require('./users')
+const commentRouter = require('./comments')
 
 router.use('/products', productRouter)
 router.use('/users', userRouter)
+router.use('/comments', commentRouter)
 
 module.exports = router;

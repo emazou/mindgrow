f07d3ecf482fa6090097b489ebeb448a09159148
Hector Garay <hectorgaraybascur@gmail.com>
2022-10-07 16:07:47 -0300

    adding payments routes

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const userRouter = require('./users')
 const commentRouter = require('./comments')
 const publicationRouter = require('./publications')
 const reviewRouter = require('./reviews')
+const paymentRouter = require('./payments')
 
 router.use('/products', productRouter)
 router.use('/users', userRouter)
 router.use('/comments', commentRouter)
 router.use('/publications', publicationRouter)
 router.use('/reviews', reviewRouter)
+router.use('/payments', paymentRouter)
 
 module.exports = router;

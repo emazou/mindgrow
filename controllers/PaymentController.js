9358b1653bc8b71e8466a27a632f09b7cb5da8f2
Hector Garay <hectorgaraybascur@gmail.com>
2022-10-07 16:08:09 -0300

    adding payment controller

---
+++ controllers/PaymentController.js
@@ -0,0 +1,33 @@
+class PaymentController {
+    constructor(subscriptionService) {
+        this.subscriptionService = subscriptionService;
+    }
+
+    async getPaymentLink(res, req) {
+        try {
+            const payment = await this.subscriptionService.createPayment();
+            return res.json(payment);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                error: true,
+                message: 'Failed to create payment'
+            })
+        }
+    }
+
+    // async getSubscriptionLink(res, req) {
+    //     try {
+    //         const subscription = await this.subscriptionService.createSubscription();
+    //         return res.json(subscription);
+    //     } catch (error) {
+    //         console.log(error);
+    //         return res.status(500).json({
+    //             error: true,
+    //             message: 'Failed to create subscription'
+    //         })
+    //     }
+    // }
+}
+
+module.exports = PaymentController;

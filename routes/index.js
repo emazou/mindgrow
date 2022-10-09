b68f3bb1d17a133fffb47872339cbd7fccfa90f4
emazou <estefania.mazou@gmail.com>
2022-10-09 17:41:02 -0500

    route modifide

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ const productRouter = require('./products')
 const userRouter = require('./users')
 const commentRouter = require('./comments')
 const publicationRouter = require('./publications')
-const reviewRouter = require('./reviews')
+const questionRouter = require('./questions')
 const paymentRouter = require('./payments')
 
 router.use('/products', productRouter)
 router.use('/users', userRouter)
 router.use('/comments', commentRouter)
 router.use('/publications', publicationRouter)
-router.use('/reviews', reviewRouter)
+router.use('/questions', questionRouter)
 router.use('/payments', paymentRouter)
 
 module.exports = router;

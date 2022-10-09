464ec020b00004301af373ab41517ef31af1e719
emazou <estefania.mazou@gmail.com>
2022-10-09 17:42:51 -0500

    schema name modified

--- models/Question.js
+++ models/Question.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const reviewSchema = new mongoose.Schema({
-    review: { type: String, required: true, min: 4, max: 300 },
+const questionSchema = new mongoose.Schema({
+    question: { type: String, required: true, min: 4, max: 300 },
     user: { type: mongoose.Types.ObjectId, ref: 'users', required: true },
     product: { type: mongoose.Types.ObjectId, ref: 'products', required: true },
 })
-const Review = mongoose.model(
-    'reviews',
-    reviewSchema
+const Question = mongoose.model(
+    'question',
+    questionSchema
 )
-module.exports = Review
+module.exports = Question
